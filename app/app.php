6e86a84c44a617609cc49caba94c1e747dce9871
🔥 Осмухин Даниил <osmuhin@web-canape.com>
2025-06-27 13:08:28 +0300

    Remove useless code & configure environment

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Foundation\Application;
+
+putenv('APP_SERVICES_CACHE=' . __DIR__ . '/../.runtime/cache/services.php');
+putenv('APP_PACKAGES_CACHE=' . __DIR__ . '/../.runtime/cache/packages.php');
+putenv('APP_CONFIG_CACHE='   . __DIR__ . '/../.runtime/cache/config.php');
+putenv('APP_ROUTES_CACHE='   . __DIR__ . '/../.runtime/cache/routes-v7.php');
+putenv('APP_EVENTS_CACHE='   . __DIR__ . '/../.runtime/cache/events.php');
+
+return Application::configure(basePath: dirname(__DIR__))
+	->withRouting(
+		commands: __DIR__.'/../routes/console.php',
+		health: '/up',
+	)
+	->create()
+	->useBootstrapPath(__DIR__);
