a72caedb6eda859d6e10eac028d30e97df73c64c
Осмухин Даниил <wischerdson@gmail.com>
2025-07-16 12:05:59 +0300

    Setting up cron

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,24 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		//
+		$this->configureCronRoute();
+	}
+
+	protected function configureCronRoute()
+	{
+		if (!$schedulerToken = env('SCHEDULER_TOKEN')) {
+			throw new RuntimeException('SCHEDULER_TOKEN environment variable is not set.');
+		}
+
+		Route::get('/schedule-run', function (Request $request) use ($schedulerToken) {
+			if (
+				($requestToken = $request->input('token')) &&
+				($requestToken === $schedulerToken)
+			) {
+				return Artisan::call('schedule:run');
+			}
+
+			abort(404);
+		});
 	}
 }
